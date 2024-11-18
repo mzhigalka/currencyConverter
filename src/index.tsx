1c830bbbecd922422e5e53bbcf6cd81b57a5a743
Matvey <129788224+mzhigalka@users.noreply.github.com>
2024-11-18 10:32:26 +0200

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from "react-dom/client";
 import React from "react";
 import App from "./App";
-
+ 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
